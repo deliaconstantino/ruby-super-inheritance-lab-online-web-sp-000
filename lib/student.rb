f4180aa5bbddd0a10b0dc945a1adef814b97fb54
Delia Constantino <dconstan92@gmail.com>
2020-11-19 20:26:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,3 @@
-class Student
-  def hello
-    puts "Hey there! I'm so excited to learn stuff."
-  end
-
-  def raise_hand
-    puts "Pick me!"
-  end
-end
-
-
-
-
-
-
-
-
-
-
-
  class Student
   def hello
     puts "Hey there! I'm so excited to learn stuff."
